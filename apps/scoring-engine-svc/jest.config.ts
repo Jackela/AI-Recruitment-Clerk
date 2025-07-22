d7a40a030b717e8052fe090dde07efd6f108a3b2
Jackela <k740724287@gmail.com>
2025-07-22 16:57:10 +1000

    feat(scoring-engine): initialize scoring microservice

---
+++ apps/scoring-engine-svc/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'scoring-engine-svc',
+  preset: '../../jest.preset.cjs',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/scoring-engine-svc',
+};
